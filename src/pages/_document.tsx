4a2c80466343c2f5234a6eda37f36aa5cd348488
tnb24 <tanaybhardwaj24@gmail.com>
2023-04-05 16:55:08 +0000

    step 2 of 4: styling heading

---
+++ src/pages/_document.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore
+
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,9 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      inter: ["Inter"],
+    },
     extend: {},
   },
   plugins: [],
